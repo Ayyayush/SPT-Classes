55ee9927a0b124cdd1a4ec17d5a8343fe3132a74
Taher Malik <tahermalik2002@gmail.com>
2025-12-10 18:12:25 +0530

    Animation changed

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,48 +1,32 @@
 import Header from "./Header.jsx"
-import { useEffect } from "react";
-import { useState } from "react";
-
+import { motion } from "framer-motion";
+export function HeroDisplay(props) {
+    return (<div className="flex flex-shrink-0 w-[100vw] items-center justify-center text-3xl font-bold bg-blue-500">
+        {props.msg}
+    </div>)
+}
 export function HeroComponent() {
-    const [index, setIndex] = useState(0);
-    const [fade, setFade] = useState(true);
-    const colorOption = {
-        0: "blue",
-        1: "green",
-        2: "orange"
-    }
 
-    const text={
-        0:"Taher",
-        1:"Malik",
-        2:"NIT JSR"
-    }
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setFade(false);
-            setTimeout(() => {
-                setIndex((prev) => (prev + 1) % 3);
-                setFade(true);
-            }, 800); 
-        }, 3000);
-
-        return () => clearInterval(interval);
-    }, []);
+    const items = ["Taher", "Ayush", "Abhishek"];
 
     return (
-        <>
-            <div className={`w-full h-[300px] `} >
-                <div className={`
-                    w-full h-[250px]
-                    transition-all duration-500
-                    ${fade ? "translate-x-0 opacity-100" : "translate-x-100 opacity-0"}
-                `}
-                style={{ background: colorOption[index] }}>
-                    {text[index]}
-
-                </div>
-            </div >
-        </>
-    )
+        <div className="w-full overflow-hidden flex h-[300px] bg-gray-200">
+            <motion.div
+                className="flex"
+                animate={{ x: ["-300vw", "0vw"] }}   // LEFT → RIGHT
+                transition={{
+                    duration: 20,
+                    ease: "linear",
+                    repeat: Infinity
+                }}
+            >
+                {/* duplicate list for infinite seamless loop */}
+                {[...items, ...items].map((msg, index) => (
+                    <HeroDisplay msg={msg} key={index} />
+                ))}
+            </motion.div>
+        </div>
+    );
 }
 export default function LandingPage() {
     return (
