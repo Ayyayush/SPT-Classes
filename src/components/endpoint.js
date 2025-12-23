ec99ebead64e450665b71044a11b00e2a2fa26d4
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-12-23 10:34:57 +0530

    Deploying-Updates

--- src/components/endpoint.js
+++ src/components/endpoint.js
@@ -1,4 +1,4 @@
-export const ADMIN_ENDPOINTS="http://localhost:5000/admin"
-export const USER_ENDPOINTS="http://localhost:5000/user"
-export const AUTH_ENDPOINTS="http://localhost:5000/api/auth"
+export const ADMIN_ENDPOINTS="https://spt-classes-backend.onrender.com/admin"
+export const USER_ENDPOINTS="https://spt-classes-backend.onrender.com/user"
+export const AUTH_ENDPOINTS="https://spt-classes-backend.onrender.com/api/auth"
 export const BASE_URL="http://localhost:3000"
